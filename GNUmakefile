7a47ca297435583aec6bd25ad57b662cbb87fd15
tenorok <ttenor@yandex.ru>
2013-05-01 00:13:55 +0400

    Грант собирает проект

--- GNUmakefile
+++ GNUmakefile
@@ -1,25 +1,11 @@
 .DEFAULT_GOAL :=
 
-BORSCHIK := ./node_modules/.bin/borschik
-UGLIFYJS := ./node_modules/.bin/uglifyjs
-
-all::
-	$(BORSCHIK) -t js -i common/.js -o common/common.js -m no
-	$(BORSCHIK) -t css -i common/.css -o common/common.css
-
-.PHONY: install
-install::
-	npm install
-	export PATH=./node_modules/.bin:$PATH
-	make
-
 .PHONY: production
 production::
 	rm -rf production
 	mkdir -p production/common
 	cp common/common.css production/common/common.css
 	cp common/common.js production/common/common.js
-	$(UGLIFYJS) -o production/common/common.js production/common/common.js
 	cp -r words/ production/words/
 	cp -r lib/ production/lib/
 	cp index.html production/index.html
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,50 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['concat', 'watch']);
+    grunt.registerTask('default', ['watch', 'concat']);
+    grunt.registerTask('production', ['concat', 'cssmin', 'uglify']);
 
     grunt.initConfig({
-        concat: {
-            js: {
-                src: ['blocks/page/page.js', 'blocks/*/*.js'],
-                dest: 'common/common.js'
-            },
-            css: {
-                src: 'blocks/*/*.css',
-                dest: 'common/common.css'
-            }
-        },
         watch: {
             scripts: {
                 files: 'blocks/*/*.js',
                 tasks: ['concat'],
                 options: {
                     debounceDelay: 1000
+                }
+            }
+        },
+        concat: {
+            css: {
+                src: 'blocks/*/*.css',
+                dest: 'common/common.css'
+            },
+            js: {
+                src: ['blocks/page/page.js', 'blocks/*/*.js'],
+                dest: 'common/common.js'
+            }
+        },
+        cssmin: {
+            compress: {
+                files: {
+                    'common/common.css': 'common/common.css'
+                },
+                options: {
+                    report: 'gzip'
+                }
+            }
+        },
+        uglify: {
+            compress: {
+                files: {
+                    'common/common.js': 'common/common.js'
+                },
+                options: {
+                    report: 'gzip'
                 }
             }
         }
